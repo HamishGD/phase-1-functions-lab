c56b1be681e30b14a696b2d955c92a1f34fec067
hamish <hamishgeorgedavie@gmail.com>
2023-03-12 20:56:57 +1000

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+//HQ BLOCKS
+//calculates distances below 42nd street
+function distanceFromHqInBlocks (someValue) {
+    if (someValue<42){
+        return(42-someValue);
+    }
+}
+
+//HQ FEET
+//calculates distances below 42nd street
+function distanceFromHqInFeet (someValue) {
+    return distanceFromHqInBlocks (someValue)*264;
+}
+
+//TRAVELFEET
+//returns the distance travelled in feet 
+function distanceTravelledInFeet(a,b) {
+    if (b>a){
+        return((b-a)*264);
+    }
+//returns a distance in feet
+    else 
+    return ((a-b*264));
+}
+
+//FAREPRICE
+//gives customers a free sample
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance<= 400){
+        return 0;
+    }
+}
+//charges 2 cent per foot..
+if (distance>= 400 && distance<= 2000){
+    return (distance-400)* 0.02;
+}
+else if (distance>= 2000 && distance<= 2500){
+    return 25;
+}
+else if (distance > 2500){
+    return "cannot travel that far";
+}
+
+
+
+
+
